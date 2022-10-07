22748ba8235c81f87e60ea5981978ff5e2a03389
DavidJoao <davidsandoval596@gmail.com>
2022-10-06 22:26:34 -0700

    initialized tuples

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-let age = 20;
+let sales = 123456789;
+let course = 'TypeScript';
+let isPublished = true;
+let level;
+level = 1;
+level = 'a';
+function render(document) {
+    console.log(document);
+}
+let numberArray = [1, 2, 3, 4];
+let stringArray = ['Hello', 'my', 'name', 'is', 'David'];
+let user = [1, 'David'];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,15 @@ function render (document: any) {
     console.log(document)
 }
 
+// When writing arrays in typescript, their type also must be declared
+let numberArray: number[] = [1,2,3,4];
+// A incorrect array would be:
+// let numberArray: number[] = [1,2,3,4,'a'];
+// An example of an array with only string values:
+let stringArray: string[] = ['Hello', 'my', 'name', 'is', 'David']
+
+// TUPLES //
+
+let user: [number, string] = [1, 'David']
+// if you write user[0]., Typescript will try to autocomplete with number methods
+// if you write user[1]., Typescript will try to autocomplete with string methods
